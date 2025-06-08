f270b245c15c71a004c0da99a08bea72e23458ab
echo <chozachel13@gmail.com>
2025-06-08 18:23:43 +0300

    add classnames, gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export enum Theme {
   LIGHT = "light",
@@ -14,7 +15,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", [theme])}>
       <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,15 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+  cls: string,
+  additional: string[],
+  mods?: Mods,
+): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([cls, value]) => Boolean(value))
+      .map(([cls, value]) => cls),
+  ].join(" ");
+}
