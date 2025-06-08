716c48c966a0e071f493eb0558b374dea5521ce5
echo <chozachel13@gmail.com>
2025-06-08 15:08:36 +0300

    add themes, styles organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import "./index.scss";
+import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/aboutPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { useTheme } from "./theme/useTheme";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
